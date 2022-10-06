264b52fb3ed60791a8cdd255a94aee1fc0252864
Yerzhan-A <84454614+Yerzhan-A@users.noreply.github.com>
2022-10-06 17:14:03 +0600

    Add files via upload

---
+++ src/main/java/com/CookCalk/calorie/CalorieApplication.java
@@ -0,0 +1,13 @@
+package com.CookCalk.calorie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalorieApplication.class, args);
+	}
+
+}
